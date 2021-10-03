fe4abb512d281c8c99f4e5535a97cb4427b8c08c
AbmaelFerreira <abmael_ninha@hotmail.com>
2021-10-03 01:05:03 -0400

    Mapeamento, Recursos criando uma Person

---
+++ src/main/java/com/worktool/personapi/entity/Phone.java
@@ -0,0 +1,28 @@
+package com.worktool.personapi.entity;
+
+import com.worktool.personapi.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+}
